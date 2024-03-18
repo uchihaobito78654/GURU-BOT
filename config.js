01b13af31d24ffb4bba43a97687f8a2fbbdadbc1
uchihaobito78654 <163227557+uchihaobito78654@users.noreply.github.com>
2024-03-18 11:41:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['916289691237', 'UCHIHA MADARA', true],
+  ['916290568766', 'UCHIHA OBITO', true], 
   [''] 
 ] //Number of owners
 
